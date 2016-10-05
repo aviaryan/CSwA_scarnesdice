b6931756eb4babca67c7d8e0dec1291f3f0bb504
aviaryan <avi.aryan123@gmail.com>
2016-10-05 08:20:52 +0530

    init project

---
+++ app/src/main/java/in/aviaryan/scarnesdice/MainActivity.java
@@ -0,0 +1,13 @@
+package in.aviaryan.scarnesdice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(in.aviaryan.scarnesdice.R.layout.activity_main);
+    }
+}
