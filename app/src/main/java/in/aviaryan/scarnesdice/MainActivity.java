071e91a49f0ef9ba12dbe157f3ac97523a6f3ed9
aviaryan <avi.aryan123@gmail.com>
2016-10-05 09:15:50 +0530

    initialized views, created basic roll dice logic

--- app/src/main/java/in/aviaryan/scarnesdice/MainActivity.java
+++ app/src/main/java/in/aviaryan/scarnesdice/MainActivity.java
@@ -2,12 +2,74 @@ package in.aviaryan.scarnesdice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtUserScore, txtComputerScore;
+    ImageView imgDice;
+    Button btnRoll, btnHold, btnReset;
+    private Random random = new Random();
+    private int currentUserScore;
+    private int diceIcons [] = {
+            R.drawable.dice1, R.drawable.dice2, R.drawable.dice3,
+            R.drawable.dice4, R.drawable.dice5, R.drawable.dice6
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(in.aviaryan.scarnesdice.R.layout.activity_main);
+        // instantiate views
+        txtUserScore = (TextView) findViewById(R.id.userScore);
+        txtComputerScore = (TextView) findViewById(R.id.computerScore);
+        imgDice = (ImageView) findViewById(R.id.diceImage);
+        btnRoll = (Button) findViewById(R.id.btnRoll);
+        btnHold = (Button) findViewById(R.id.btnHold);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        // start game
+        // onStart()
+        // event listeners
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStart();
+            }
+        });
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+        btnHold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void rollDice(){
+        int num = random.nextInt(6) + 1;
+        int currentScore = Integer.parseInt(txtUserScore.getText().toString());
+        imgDice.setImageResource(diceIcons[num-1]);
+        currentScore += num;
+        currentUserScore += num;
+        txtUserScore.setText(currentScore + "");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUserScore = 0;
+        txtUserScore.setText("0");
+        txtComputerScore.setText("0");
     }
 }
